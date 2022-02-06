672a9ef4d7cc0e5f8ae02cc0286443b694acbcc4
killinsun <noneck.0311@gmail.com>
2022-02-06 19:48:12 +0900

    add env path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "/usr/local/src/controlHumidifier/.env" });
 
 interface AxiosResponseHumidStat {
   statusCode: string;
